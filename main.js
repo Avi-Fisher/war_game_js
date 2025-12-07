c4f78a6bfc1ea9298811f09047f4b46edc71e711
Or200 <avimeda74@gmail.com>
2025-12-07 11:56:29 +0200

    add create_card func

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// hello
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const suites = ["H", "C", "D", "S"];
+
+function create_card(rank, suite) {
+  const specialCards = [11, 12, 13, 14];
+  viewRank = rank;
+
+  if (specialCards.includes(rank)) {
+    switch (rank) {
+      case 11:
+        viewRank = "J";
+        break;
+      case 12:
+        viewRank = "Q";
+        break;
+      case 13:
+        viewRank = "K";
+        break;
+      case 14:
+        viewRank = "A";
+        break;
+    }
+  }
+  return { rank: viewRank, suite: suite, value: rank };
+}
+
+
+
